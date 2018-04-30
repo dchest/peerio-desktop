163f17df932446e0d083f86fe9e9afe70165e91d
suprko <lucas@peerio.com>
2018-04-30 17:12:05 -0400

    WIP: PendingDM.jsx (empty right now)

---
+++ src/ui/chat/components/PendingDM.jsx
@@ -0,0 +1,11 @@
+const React = require('react');
+
+const { action, observable } = require('mobx');
+const { observer } = require('mobx-react');
+
+@observer
+class PendingDM extends React.Component {
+
+}
+
+module.exports = PendingDM;
