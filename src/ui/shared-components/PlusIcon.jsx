f1e5ab5e9a65427a3c19e8e25614ef6fbd4ba177
suprko <lucas@peerio.com>
2018-05-01 14:16:13 -0400

    chore: add new PlusIcon component

---
+++ src/ui/shared-components/PlusIcon.jsx
@@ -0,0 +1,12 @@
+// TODO: use this component in ChatList and ZeroChats
+const React = require('react');
+
+class PlusIcon extends React.PureComponent {
+    render() {
+        return (
+            <span className="plus-icon">+</span>
+        );
+    }
+}
+
+module.exports = PlusIcon;
