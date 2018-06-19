23b0c4c96a7c45167f6e68040ece3fab08aad838
Dmitry Chestnykh <dmitry@codingrobots.com>
2018-06-19 13:29:04 +0200

    chore: automatically install failed update if it failed only once

--- src/main-process/updater.js
+++ src/main-process/updater.js
@@ -47,11 +47,11 @@ function start(mainWindow) {
         ipcMain.on('update-check-last-failed', async () => {
             if (!autoUpdater) return;
 
-            const failed = await autoUpdater.didLastUpdateFail();
-            if (!failed) {
+            const failedAttempts = await autoUpdater.failedInstallAttempts();
+            if (!failedAttempts) {
                 autoUpdater.cleanup(); // don't care to await it
             }
-            mainWindow.webContents.send('update-last-failed', failed);
+            mainWindow.webContents.send('update-failed-attempts', failedAttempts);
         });
 
         ipcMain.on('update-cleanup', async () => {
